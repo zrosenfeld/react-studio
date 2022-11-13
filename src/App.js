e79e220157a2d572f6356978b51ed0f3dbd384ec
Zachary Rosenfeld <zacharyrosenfeld@zacharys-mbp-3.devices.brown.edu>
2022-11-13 17:39:10 -0500

    finished lab

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { useState } from "react";
 import bakeryData from "./assets/bakery-data.json";
+import BakeryItem from "./components/BakeryItem";
 
 /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
 bakeryData.forEach((item) => {
@@ -11,18 +12,38 @@ bakeryData.forEach((item) => {
 function App() {
   // TODO: use useState to create a state variable to hold the state of the cart
   /* add your cart state code here */
+  let [cartItems, setCartItems] = useState([]);
+  let [price, setPrice] = useState(0);
 
   return (
     <div className="App">
       <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
-
-      {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
-      ))}
-
+      {bakeryData.map(
+        (
+          item,
+          index // TODO: map bakeryData to BakeryItem components
+        ) => (
+          <BakeryItem
+            item={item}
+            index={index}
+            cartItems={cartItems}
+            setCartItems={setCartItems}
+            price={price}
+            setPrice={setPrice}
+          /> // replace with BakeryItem component
+        )
+      )}
       <div>
-        <h2>Cart</h2>
-        {/* TODO: render a list of items in the cart */}
+        <h1>Cart</h1>
+        {cartItems.map(
+          (
+            item,
+            index // TODO: map bakeryData to BakeryItem components
+          ) => (
+            <h2>{item.name}</h2>
+          )
+        )}
+        <h2>Total: {price}</h2>
       </div>
     </div>
   );
--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,24 @@
-// TODO: create a component that displays a single bakery item
+function BakeryItem(props) {
+  const handleClick = () => {
+    const newList = props.cartItems.concat(props.item);
+    props.setCartItems(newList);
+    props.setPrice(props.price + props.item.price);
+  };
+  return (
+    <div>
+      <img class="image" src={props.item.image} />
+      <h2>{props.item.name}</h2>
+      <h3>{props.item.description}</h3>
+      <div class="row">
+        <img
+          onClick={handleClick}
+          class="column"
+          src="https://cdn-icons-png.flaticon.com/512/263/263142.png"
+        />
+        <h1>{props.item.price}</h1>
+      </div>
+    </div>
+  );
+}
+
+export default BakeryItem;
